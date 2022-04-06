3383dac1e6b4861570f00b8f3ed06c417a372dc9
text2vijay <text2vijay@hotmail.com>
2022-04-06 17:27:32 +0530

    marketing remoteEntry.js path setup

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -73,7 +73,7 @@ export default function Album() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              vijay Home Page
+              Home Page
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don&apos;t simply skip over it entirely.
