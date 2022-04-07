418e7133fb2d9844ece80036fd41942c9a5da21a
text2vijay <text2vijay@hotmail.com>
2022-04-07 10:20:28 +0530

    router navigation settings using memory history and browser histroy

--- packages/container/src/component/MarketingApp.js
+++ packages/container/src/component/MarketingApp.js
@@ -1,13 +1,23 @@
 import { mount } from "marketing/MarketingApp";
 
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        console.log("the container noticed navigation in marketing", nextPathname);
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) history.push(nextPathname);
+      },
+    });
 
+    history.listen(onParentNavigate);
+  }, []);
+  //useeffect need to run when marketing app render for first time
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom";
-
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 //Mount function to start up the app
-const mount = (e1) => {
-  ReactDom.render(<App />, e1);
+const mount = (e1, { onNavigate, defaultHistrory }) => {
+  const history = defaultHistrory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDom.render(<App history={history} />, e1);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log("container just navigated");
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 //if we are in dev mode and in isolation
@@ -13,7 +27,7 @@ const mount = (e1) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistrory: createBrowserHistory() });
   }
 }
 
